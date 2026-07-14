d36e5647b682501bb398abe2fb48e618b6cb10dd
vincent1bt <vincent14319@gmail.com>
2026-07-13 20:03:18 -0600

    Pipeline runner changed tag

--- pipeline_runner.py
+++ pipeline_runner.py
@@ -1,14 +1,19 @@
 from google.cloud import aiplatform
 import os
+import logging
 
 PROJECT_ID = os.getenv('PROJECT_ID')
 LOCATION = os.getenv('REGION')
 RUNNER_SERVICE_ACCOUNT = os.getenv('SERVICE_ACCOUNT')
 GITHUB_SHA = os.getenv('GITHUB_SHA')
 
-TEMPLATE_LOCATION = "https://us-central1-kfp.pkg.dev/{PROJECT_ID}/pipelines-repository/pytorch-test-pipeline/22bb7de88638"
+TEMPLATE_LOCATION = "https://us-central1-kfp.pkg.dev/{PROJECT_ID}/pipelines-repository/pytorch-test-pipeline/latest"
 
 aiplatform.init(project=PROJECT_ID, location=LOCATION)
+
+logging.getLogger().setLevel(logging.INFO)
+
+logging.info(f"TEMPLATE USED: {TEMPLATE_LOCATION}")
 
 job = aiplatform.PipelineJob(
     display_name="pytorch test pipeline run",
